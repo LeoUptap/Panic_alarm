60cb3fd835bc1b1b4fc474c96c8f8d1db44aa88d
LeoGG26 <garnicaelvis76@gmail.com>
2025-07-20 20:04:16 -0600

    error ortografico en main

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 from Database import engine,create_db_and_tables
 from sqlmodel import SQLModel
 
-from controller.user_controller import router as user_router
-#from controller.location_controller import router as location_router
-#from controller.emergency_controller import router as emergency_router
+from controller.User_controller import router as user_router
+from controller.Location_controller import router as location_router
+from controller.Emergency_number_controller import router as emergency_router
 
 app = FastAPI()
 
@@ -16,5 +16,5 @@ def on_startup():
 
 # Registrar routers
 app.include_router(user_router, prefix="/users", tags=["Users"])
-#app.include_router(location_router, prefix="/locations", tags=["Locations"])
-#app.include_router(emergency_router, prefix="/emergencies", tags=["Emergency Numbers"])
+app.include_router(location_router, prefix="/locations", tags=["Locations"])
+app.include_router(emergency_router, prefix="/emergencies", tags=["Emergency Numbers"])
